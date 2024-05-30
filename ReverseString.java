b4f65fc9a3fcdb3f575972f7ee751e7b34a7abfe
Nishanish2024 <161607747+Nishanish2024@users.noreply.github.com>
2024-05-30 09:16:44 +0300

    Add files via upload

--- ReverseString.java
+++ ReverseString.java
@@ -1,30 +1,21 @@
-package Java.challenge;
+package week1.day4;
 
 public class ReverseString {
-	/*
-	 * Input: s = "  hello world  "
-Output: "world hello"
-Explanation: Your reversed string should not contain leading or trailing spaces.
-	 */
-	
-	public static String reverseWords(String s) {
-		String[] arr = s.trim().split("\\s+");
-		int i = 0;
-		int j = arr.length - 1;
-		while (i < j) {
-			String temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-			i++;
-			j--;
-		}
-		return String.join(" ", arr);
+
+	public static void main(String[] args) {
+		String companyName="TestLeaf";
+		reverseString(companyName);
 	}
 	
-	public static void main(String[] args) {
-		String s= "  hello world  ";
-		System.out.println("Input String is:"+ s);
-		String output= reverseWords(s);
-		System.out.println("Reversed string is: "+ output);
+	public static String reverseString(String input) 
+	{
+		String reverse="";
+		System.out.print("Input is: "+input);
+		char[] charArray=input.toCharArray();
+		for (int i = charArray.length-1; i >=0 ; i--) {
+			reverse=reverse+charArray[i];
+		}
+		System.out.print("\nReversed String is: "+reverse);
+		return reverse;
 	}
 }
